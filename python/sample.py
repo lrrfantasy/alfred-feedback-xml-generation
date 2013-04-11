8492b268d55192d87ccbb76140e6ef16c878a740
Ruoran Liu <rrliu@thoughtworks.com>
2013-04-11 17:47:23 +0800

    Improved Python template to support type attribute

--- python/sample.py
+++ python/sample.py
@@ -1,5 +1,5 @@
 from Feedback import Feedback
 fb = Feedback()
 fb.add_item("Hello", "World")
-fb.add_item("Foo", subtitle="Bar", icon="foobar.icon", arg="foobar")
+fb.add_item("Foo", subtitle="Bar", arg="foobar", valid = "no", autocomplete = "complete", icon="public.jpeg", filetype="file", icontype="filetype")
 print fb
