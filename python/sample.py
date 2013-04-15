cf817b7484525ae82b1dd5bb0317e5cf93c9631a
Ruoran Liu <rrliu@thoughtworks.com>
2013-04-15 09:41:13 +0800

    Fix Python typo

--- python/sample.py
+++ python/sample.py
@@ -1,5 +1,5 @@
 from Feedback import Feedback
 fb = Feedback()
 fb.add_item("Hello", "World")
-fb.add_item("Title", subtitle="Item description", arg="Value return to workflow", valud="yes", autocomplete="Description", icon="public.jpeg", filetype="file", icontype="filetype")
+fb.add_item("Title", subtitle="Item description", arg="Value return to workflow", valid="yes", autocomplete="Description", icon="public.jpeg", filetype="file", icontype="filetype")
 print fb
